1f28e8a6c221a8163ee3cb16ddd337c8fe7694f8
pyrimidine <35439681+pyrimidine@users.noreply.github.com>
2023-05-27 16:54:26 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -10,7 +10,6 @@ from PIL import Image
 from torchvision import transforms
 from math import exp
 # 5.SSIM loss
-# 生成一位高斯权重，并将其归一化
 def gaussian(window_size,sigma):
     gauss = torch.Tensor([exp(-(x - window_size // 2) ** 2 / float(2 * sigma ** 2)) for x in range(window_size)])
     return gauss/torch.sum(gauss)  # 归一化
@@ -22,13 +21,9 @@ def gaussian(window_size,sigma):
 # print(x.shape) #torch.Size([3,1])
 # print(x.t().unsqueeze(0).unsqueeze(0).shape) # torch.Size([1,1,1, 3])
 
-# 生成滑动窗口权重，创建高斯核：通过一维高斯向量进行矩阵乘法得到
 def create_window(window_size,channel=1):
     _1D_window = gaussian(window_size, 1.5).unsqueeze(1)  # window_size,1
-    # mm:矩阵乘法 t:转置矩阵 ->1,1,window_size,_window_size
     _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
-    # expand:扩大张量的尺寸，比如3,1->3,4则意味将输入张量的列复制四份，
-    # 1,1,window_size,_window_size->channel,1,window_size,_window_size
     window = Variable(_2D_window.expand(channel, 1, window_size, window_size).contiguous())
     return window
 
@@ -56,7 +51,6 @@ def _ssim(img1, img2, window, window_size, channel, size_average=True):
         return ssim_map.mean(1).mean(1).mean(1)
 
 
-# 构造损失函数用于网络训练或者普通计算SSIM值
 class SSIM(torch.nn.Module):
     def __init__(self, window_size=11, size_average=True):
         super(SSIM, self).__init__()
@@ -83,7 +77,6 @@ class SSIM(torch.nn.Module):
         return _ssim(img1, img2, window, self.window_size, channel, self.size_average)
 
 
-# 普通计算SSIM
 def ssim(img1, img2, window_size=11, size_average=True):
     (_, channel, _, _) = img1.size()
     window = create_window(window_size, channel)
